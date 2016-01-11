ca82add1f6400eb3a894b62a801284c0fdd95652
BruceGUO <cqdeyouxiang@163.com>
2016-01-11 23:38:34 +0800

    first commit

---
+++ BRSearchViewTestDemo/BRSearchModel.h
@@ -0,0 +1,13 @@
+//
+//  BRSearchModel.h
+//  BRSearchViewTestDemo
+//
+//  Created by Bruce on 16/1/11.
+//  Copyright © 2016年 Bruce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BRSearchModel : NSObject
+
+@end
