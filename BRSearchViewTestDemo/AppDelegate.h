9b0704afc2d5d8ad24698d12e2166e3ecbc94853
BruceGUO <cqdeyouxiang@163.com>
2016-01-11 09:50:43 +0800

    Initial Commit

---
+++ BRSearchViewTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRSearchViewTestDemo
+//
+//  Created by Bruce on 16/1/11.
+//  Copyright © 2016年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
